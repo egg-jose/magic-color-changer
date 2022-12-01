8f6a9b69c3cfdc9b2d80f8849819f28c9478d90b
Jose Esteban <josegg1897@gmail.com>
2022-12-01 03:19:14 -0400

    Change magic color page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container" :style="{background: color}">
+    <Card @change-random-color="changeRandomColor" :color="color" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "./components/Card.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      color: "#ffffaa",
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    Card,
+  },
+  methods: {
+    changeRandomColor() {
+      this.color = "#" + Math.floor(Math.random()*16777215).toString(16);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+@import url("https://fonts.googleapis.com/css2?family=Lexend+Deca&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Lexend Deca", sans-serif;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.btn {
+  display: inline-block;
+  background: #0a2847;
+  color: #e9e9e9;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:active {
+  transform: scale(0.98);
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="card">
+    <h3>Change magic color</h3>
+    <Button @btn-click="$emit('change-random-color')" text="Click me!" />
+    <span @click="copyURL(color)" class="colorText">{{ color }}</span>
+  </div>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+  name: "Card",
+  props: {
+    color: String,
+  },
+  components: {
+    Button,
+  },
+  methods: {
+  async copyURL(mytext) {
+    await navigator.clipboard.writeText(mytext);
+    alert('Copied');
+  }
+}
+};
+</script>
+
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  text-align: center;
+  border: 8px solid #0a2847;
+  border-radius: 20px;
+  padding: 40px;
+  background-color: #f0ffff;
+}
+h3 {
+  margin-bottom: 10px;
+}
+.colorText {
+  margin-top: 10px;
+  border: 3px solid #0a2847;
+  border-radius: 10px;
+  padding: 5px;
+  text-align: center;
+}
+.colorText:hover {
+  cursor: pointer;
+  background-color: #c2c7c7;
+}
+</style>
